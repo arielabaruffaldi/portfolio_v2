d126f3155361e65e928043653b8ba28875bb50cd
apbaruffaldi <100204802+apbaruffaldi@users.noreply.github.com>
2024-11-25 14:31:19 -0300

    chore: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { Main } from "@/modules/Main/Main";
 import { AppProvider } from "./providers/AppProvider/AppProvider";
 import { About } from "./modules/About/About";
 import { Projects } from "./modules/Projects/Projects";
+import { Footer } from "./components/templates/Footer/Footer";
 
 function App() {
   return (
     <AppProvider>
       <Nav />
       <Main />
-      <Projects />
-      <About />
+     {/*  <Projects />
+      <About /> */}
+      <Footer />
     </AppProvider>
   );
 }
