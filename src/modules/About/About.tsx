a0a13c14c510ea37533bbbcf941be3580b165d42
apbaruffaldi <100204802+apbaruffaldi@users.noreply.github.com>
2024-11-25 14:30:59 -0300

    feat(About): add about section

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -1,7 +1,43 @@
 import { FunctionComponent } from "react";
 
 import styles from "./About.module.scss";
+import { Text } from "@/components/atoms/Text/Text";
+import { SectionWrapper } from "@/components/templates/SectionWrapper/SectionWrapper";
+import { Skills } from "./components/Skills/Skills";
 
 export const About: FunctionComponent = () => {
-  return <section id="about" className={styles.about}></section>;
+  return (
+    <SectionWrapper
+      title="About"
+      id="about"
+      className={styles.about}
+      hasPadding
+    >
+      {/*  <div className={styles.about__description}>
+        <Text size="lg" textColor="gray">
+          I am a{" "}
+          <Text as="span" size="lg" fontStyle="italic">
+            {" "}
+            Frontend Developer
+          </Text>{" "}
+          with a passion for creatin{" "}
+          <Text as="span" size="lg" fontStyle="italic">
+            beautiful and functional user interfaces.
+          </Text>{" "}
+        </Text>
+        <Text size="lg" textColor="gray">
+          I have experience working with modern technologies such as
+          <Text as="span" size="lg" fontStyle="italic">
+            {" "}
+            React, Redux, Next.js, and Tailwind CSS.
+          </Text>{" "}
+        </Text>
+        <Text size="lg" textColor="gray">
+          I am always looking for new challenges and opportunities to grow as a
+          developer.
+        </Text>
+      </div> */}
+      <Skills />
+    </SectionWrapper>
+  );
 };
